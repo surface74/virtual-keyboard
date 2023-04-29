700e538be0ed1cb7fc0ac0f09f1ccd4302dd15a5
surface74 <warwhisper@tut.by>
2023-04-29 21:52:58 +0300

    fix: correct key-set

--- src/js/key-set.js
+++ src/js/key-set.js
@@ -2,43 +2,43 @@ export default class KeySet {
   get keys() {
     return {
       0: {
-        keyRu: 'ё', keyRuShift: 'Ё', keyEn: '`', keyEnShift: '', code: 'Backquote', keyCode: 192,
+        keyRu: 'ё', keyRuShift: 'Ё', keyEn: '`', keyEnShift: '~', code: 'Backquote', keyCode: 192,
       },
       1: {
-        keyRu: '1', keyRuShift: '!', keyEn: '1', keyEnShift: '', code: 'Digit1', keyCode: 49,
+        keyRu: '1', keyRuShift: '!', keyEn: '1', keyEnShift: '!', code: 'Digit1', keyCode: 49,
       },
       2: {
-        keyRu: '2', keyRuShift: '"', keyEn: '2', keyEnShift: '', code: 'Digit2', keyCode: 50,
+        keyRu: '2', keyRuShift: '"', keyEn: '2', keyEnShift: '@', code: 'Digit2', keyCode: 50,
       },
       3: {
-        keyRu: '3', keyRuShift: '№', keyEn: '3', keyEnShift: '', code: 'Digit3', keyCode: 51,
+        keyRu: '3', keyRuShift: '№', keyEn: '3', keyEnShift: '#', code: 'Digit3', keyCode: 51,
       },
       4: {
-        keyRu: '4', keyRuShift: ';', keyEn: '4', keyEnShift: '', code: 'Digit4', keyCode: 52,
+        keyRu: '4', keyRuShift: ';', keyEn: '4', keyEnShift: '$', code: 'Digit4', keyCode: 52,
       },
       5: {
-        keyRu: '5', keyRuShift: '%', keyEn: '5', keyEnShift: '', code: 'Digit5', keyCode: 53,
+        keyRu: '5', keyRuShift: '%', keyEn: '5', keyEnShift: '%', code: 'Digit5', keyCode: 53,
       },
       6: {
-        keyRu: '6', keyRuShift: ':', keyEn: '6', keyEnShift: '', code: 'Digit6', keyCode: 54,
+        keyRu: '6', keyRuShift: ':', keyEn: '6', keyEnShift: '^', code: 'Digit6', keyCode: 54,
       },
       7: {
-        keyRu: '7', keyRuShift: '?', keyEn: '8', keyEnShift: '', code: 'Digit7', keyCode: 56,
+        keyRu: '7', keyRuShift: '?', keyEn: '7', keyEnShift: '&', code: 'Digit7', keyCode: 56,
       },
       8: {
-        keyRu: '8', keyRuShift: '*', keyEn: '8', keyEnShift: '', code: 'Digit8', keyCode: 57,
+        keyRu: '8', keyRuShift: '*', keyEn: '8', keyEnShift: '*', code: 'Digit8', keyCode: 57,
       },
       9: {
-        keyRu: '9', keyRuShift: '(', keyEn: '9', keyEnShift: '', code: 'Digit9', keyCode: 57,
+        keyRu: '9', keyRuShift: '(', keyEn: '9', keyEnShift: '(', code: 'Digit9', keyCode: 57,
       },
       10: {
-        keyRu: '0', keyRuShift: ')', keyEn: '0', keyEnShift: '', code: 'Digit0', keyCode: 48,
+        keyRu: '0', keyRuShift: ')', keyEn: '0', keyEnShift: ')', code: 'Digit0', keyCode: 48,
       },
       11: {
-        keyRu: '-', keyRuShift: '_', keyEn: '-', keyEnShift: '', code: 'Minus', keyCode: 189,
+        keyRu: '-', keyRuShift: '_', keyEn: '-', keyEnShift: '_', code: 'Minus', keyCode: 189,
       },
       12: {
-        keyRu: '=', keyRuShift: '+', keyEn: '=', keyEnShift: '', code: 'Equal', keyCode: 187,
+        keyRu: '=', keyRuShift: '+', keyEn: '=', keyEnShift: '+', code: 'Equal', keyCode: 187,
       },
       13: {
         keyRu: 'Backspace', keyRuShift: 'Backspace', keyEn: 'Backspace', keyEnShift: 'Backspace', code: 'Backspace', keyCode: 8,
@@ -63,28 +63,28 @@ export default class KeySet {
         keyRu: 'е', keyRuShift: 'Е', keyEn: 't', keyEnShift: 'T', code: 'KeyT', keyCode: 84,
       },
       20: {
-        keyRu: 'е', keyRuShift: 'Е', keyEn: 't', keyEnShift: 'T', code: 'KeyT', keyCode: 84,
-      },
-      21: {
         keyRu: 'н', keyRuShift: 'Н', keyEn: 'y', keyEnShift: 'Y', code: 'KeyY', keyCode: 89,
       },
-      22: {
+      21: {
         keyRu: 'г', keyRuShift: 'Г', keyEn: 'u', keyEnShift: 'U', code: 'KeyU', keyCode: 85,
       },
-      23: {
+      22: {
         keyRu: 'ш', keyRuShift: 'Ш', keyEn: 'i', keyEnShift: 'I', code: 'KeyI', keyCode: 73,
       },
-      24: {
+      23: {
         keyRu: 'щ', keyRuShift: 'Щ', keyEn: 'o', keyEnShift: 'O', code: 'KeyO', keyCode: 79,
       },
-      25: {
+      24: {
         keyRu: 'з', keyRuShift: 'З', keyEn: 'p', keyEnShift: 'P', code: 'KeyP', keyCode: 80,
       },
-      26: {
+      25: {
         keyRu: 'х', keyRuShift: 'Х', keyEn: '[', keyEnShift: '{', code: 'BracketLeft', keyCode: 219,
       },
-      27: {
+      26: {
         keyRu: 'ъ', keyRuShift: 'Ъ', keyEn: ']', keyEnShift: '}', code: 'BracketRight', keyCode: 221,
+      },
+      27: {
+        keyRu: '\\', keyRuShift: '|', keyEn: '\\', keyEnShift: '/', code: 'Backslash', keyCode: 220,
       },
       28: {
         keyRu: 'Del', keyRuShift: 'Del', keyEn: 'Del', keyEnShift: 'Del', code: 'Delete', keyCode: 46,
