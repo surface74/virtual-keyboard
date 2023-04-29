27fe6cd6e8b8e5b4d0c59bd0f1cfe3c45c7e8d3d
surface74 <warwhisper@tut.by>
2023-04-29 15:05:28 +0300

    feat: add string for diff locale

---
+++ src/js/strings.js
@@ -0,0 +1,14 @@
+export default class Strings {
+  get strings() {
+    return {
+      "Ru": {
+        "tooltipOS": "Клавиатура создана в операционной системе %1%",
+        "tooltipHotKeys": "Для переключения языка комбинация: левыe Ctrl + Alt"
+      },
+      "En": {
+        "tooltipOS": "Keyboard was created in OS %1%",
+        "tooltipHotKeys": "To switch locale press: left Ctrl + Alt"
+      }
+    }
+  }
+}
