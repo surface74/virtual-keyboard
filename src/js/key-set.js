eb01f34ba46e03bdebfc10c6a12d052b9dfb14b3
surface74 <warwhisper@tut.by>
2023-04-29 15:07:59 +0300

    feat: add temlate to store buttons' info

---
+++ src/js/key-set.js
@@ -0,0 +1,48 @@
+export default class KeySet {
+  get keys() {
+    return {
+      0: {
+        keyRu: 'ё', keyRuShift: 'Ё', keyEn: '`', keyEnShift: '', code: 'Backquote', keyCode: 192,
+      },
+      1: {
+        keyRu: '1', keyRuShift: '!', keyEn: '1', keyEnShift: '', code: 'Digit1', keyCode: 49,
+      },
+      2: {
+        keyRu: '2', keyRuShift: '"', keyEn: '2', keyEnShift: '', code: 'Digit2', keyCode: 50,
+      },
+      3: {
+        keyRu: '3', keyRuShift: '№', keyEn: '3', keyEnShift: '', code: 'Digit3', keyCode: 51,
+      },
+      4: {
+        keyRu: '4', keyRuShift: ';', keyEn: '4', keyEnShift: '', code: 'Digit4', keyCode: 52,
+      },
+      5: {
+        keyRu: '5', keyRuShift: '%', keyEn: '5', keyEnShift: '', code: 'Digit5', keyCode: 53,
+      },
+      6: {
+        keyRu: '6', keyRuShift: ':', keyEn: '6', keyEnShift: '', code: 'Digit6', keyCode: 54,
+      },
+      7: {
+        keyRu: '7', keyRuShift: '?', keyEn: '8', keyEnShift: '', code: 'Digit7', keyCode: 56,
+      },
+      8: {
+        keyRu: '8', keyRuShift: '*', keyEn: '8', keyEnShift: '', code: 'Digit8', keyCode: 57,
+      },
+      9: {
+        keyRu: '9', keyRuShift: '(', keyEn: '9', keyEnShift: '', code: 'Digit9', keyCode: 57,
+      },
+      10: {
+        keyRu: '0', keyRuShift: ')', keyEn: '0', keyEnShift: '', code: 'Digit0', keyCode: 48,
+      },
+      11: {
+        keyRu: '-', keyRuShift: '_', keyEn: '-', keyEnShift: '', code: 'Minus', keyCode: 189,
+      },
+      12: {
+        keyRu: '=', keyRuShift: '+', keyEn: '=', keyEnShift: '', code: 'Equal', keyCode: 187,
+      },
+      13: {
+        keyRu: 'Backspace', keyRuShift: 'Backspace', keyEn: 'Backspace', keyEnShift: 'Backspace', code: 'Backspace', keyCode: 8,
+      },
+    };
+  }
+}
